d00584630097e1ff1a03863e3b1e1097980f598b
jm.kim <>
2023-08-29 10:37:00 +0900

    뒤로가기 시 앱 밖으로 나가는 현상 수정

--- app/src/main/java/kr/jm/composewebview/MainActivity.kt
+++ app/src/main/java/kr/jm/composewebview/MainActivity.kt
@@ -33,7 +33,7 @@ import kr.jm.composewebview.ui.theme.ComposeWebViewTheme
 
 class MainActivity : ComponentActivity() {
 
-    val viewModel by viewModels<MainViewModel>()
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
