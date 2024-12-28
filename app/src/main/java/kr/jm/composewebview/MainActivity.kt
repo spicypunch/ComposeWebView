6997739df92d61d380da95fb64332db4a00debbc
spicypunch <miniguni66@naver.com>
2024-12-28 23:41:14 +0900

    SSL 에러 해결

--- app/src/main/java/kr/jm/composewebview/MainActivity.kt
+++ app/src/main/java/kr/jm/composewebview/MainActivity.kt
@@ -1,7 +1,10 @@
 package kr.jm.composewebview
 
 import android.annotation.SuppressLint
+import android.net.http.SslError
 import android.os.Bundle
+import android.webkit.SslErrorHandler
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
@@ -133,8 +136,16 @@ fun rememberWebView(): WebView {
     val context = LocalContext.current
     val webView = remember {
         WebView(context).apply {
-            settings.javaScriptEnabled = true
-            webViewClient = WebViewClient()
+            settings.apply {
+                javaScriptEnabled = true
+                domStorageEnabled = true
+                mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            }
+            webViewClient = object : WebViewClient() {
+                override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                    handler?.proceed()
+                }
+            }
             loadUrl("https://velog.io/@godmin66")
         }
     }
